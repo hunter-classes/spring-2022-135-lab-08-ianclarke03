4385fea943605a703cd95a450025f60b00f8a5ed
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-03-24 23:27:19 +0000

    b

--- funcs.cpp
+++ funcs.cpp
@@ -23,7 +23,7 @@ void invert(std::string input){
 
 }
 
-//Task B
+/*Task B
 
 void inverthalf(std::string input){
 
@@ -113,4 +113,87 @@ void pixelate(std::string input){
   }
   
   writeImage("pixelate.pgm",pixel, h, w);
+}
+*/
+
+
+int invertHalf(std::string filename)
+{
+  std::string input = filename;
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w); // read it from the file "inImage.pgm"
+  // h and w were passed by reference and
+  // now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+  // Now we can manipulate the image the way we like
+  // for example we copy its contents into a new array
+  int out[MAX_H][MAX_W];
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      if (col >= w/2) {
+        out[row][col] = 255-img[row][col];
+      }
+      else {
+        out[row][col] = img[row][col];
+      }
+    }
+  }
+  // and save this new image to file "outImage.pgm"
+  writeImage("halfInvertedImage.pgm",out, h, w);
+  return 0;
+}
+
+int box(std::string filename)
+{
+  std::string input = filename;
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w); // read it from the file "inImage.pgm"
+  // h and w were passed by reference and
+  // now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+  // Now we can manipulate the image the way we like
+  // for example we copy its contents into a new array
+  int out[MAX_H][MAX_W];
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      if (row < h*3/4 && row > h/4 && col > w/4 && col < w*3/4) {
+        out[row][col] = 255;
+      }
+      else {
+        out[row][col] = img[row][col];
+      }
+    }
+  }
+  // and save this new image to file "outImage.pgm"
+  writeImage("boxImage.pgm",out, h, w);
+  return 0;
+}
+
+int frame(std::string filename)
+{
+  std::string input = filename;
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w); // read it from the file "inImage.pgm"
+  // h and w were passed by reference and
+  // now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+  // Now we can manipulate the image the way we like
+  // for example we copy its contents into a new array
+  int out[MAX_H][MAX_W];
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      if ((row <= h*3/4 && row >= h/4 && col >= w/4 && col <= w*3/4) && (row == h*3/4 || row == h/4 || col == w/4 || col == w*3/4)) {
+        out[row][col] = 255;
+      }
+      else {
+        out[row][col] = img[row][col];
+      }
+    }
+  }
+  // and save this new image to file "outImage.pgm"
+  writeImage("frameImage.pgm",out, h, w);
+  return 0;
 }
